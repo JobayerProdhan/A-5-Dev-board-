8341896ddd4e358235d2aafbed40a358b165017e
Jobayer Prodhan <jobayerprodhan08@gmail.com>
2025-03-04 17:15:05 +0600

    all done

--- scripts/time.js
+++ scripts/time.js
@@ -8,9 +8,9 @@ function updateClock() {
     const dateString = `${day} ${month} ${year}`; 
 
     
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const hours = now.getHours().toString()
+    const minutes = now.getMinutes().toString()
+    const seconds = now.getSeconds().toString()
 
     const timeString = `${hours}:${minutes}:${seconds}`; 
 
